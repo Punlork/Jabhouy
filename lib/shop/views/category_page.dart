f99462c3d02db9ae2cafb2ab5935afd83e440bff
Punlork <chekpunlork9999@gmail.com>
2025-03-11 17:33:23 +0700

    update category card

--- lib/shop/views/category_page.dart
+++ lib/shop/views/category_page.dart
@@ -86,7 +86,12 @@ class _CategoryPageContent extends StatelessWidget {
 
   Widget _buildCategoryTile(BuildContext context, CategoryItemModel item) {
     return Card(
-      margin: const EdgeInsets.only(bottom: 8),
+      margin: const EdgeInsets.only(bottom: 12),
+      color: Colors.white,
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
       child: ListTile(
         contentPadding: const EdgeInsets.only(left: 12),
         title: Text(
